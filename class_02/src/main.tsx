83e75f656fff460cd538382fb3d9a25e48600015
unknown <kecamposso@est.utn.ac.cr>
2025-06-20 20:20:46 -0600

    inical

---
+++ class_02/src/main.tsx
@@ -0,0 +1,21 @@
+
+import Procesar from './pages/procesar.tsx'
+
+import ReactDOM from 'react-dom/client';
+			import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+			import './index.css'
+			import App from './App.tsx'
+
+			const root = ReactDOM.createRoot(
+				document.getElementById('root') as HTMLElement
+			);
+
+			root.render(
+				<BrowserRouter>
+					<Routes>
+            <Route path="/" element={<App />} />
+						<Route path="/procesar" element={<Procesar />} />
+					</Routes>
+				</BrowserRouter>
+			);
